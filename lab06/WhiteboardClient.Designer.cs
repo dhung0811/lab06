f6ee4524662ef665ec5d662583e5f74025b1f0b2
dhung0811 <dhung81104@gmail.com>
2024-06-22 23:39:36 +0700

    Add project files.

---
+++ lab06/WhiteboardClient.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace lab06
+{
+    partial class WhiteboardClient
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnThickness;
+        private System.Windows.Forms.TextBox thicknessTextBox;
+        private System.Windows.Forms.TextBox txtImageUrl;
+        private System.Windows.Forms.Button btnInsertImage;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnThickness = new System.Windows.Forms.Button();
+            this.thicknessTextBox = new System.Windows.Forms.TextBox();
+            this.txtImageUrl = new System.Windows.Forms.TextBox();
+            this.btnInsertImage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(713, 444);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 1;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(12, 444);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnThickness
+            // 
+            this.btnThickness.Location = new System.Drawing.Point(197, 444);
+            this.btnThickness.Name = "btnThickness";
+            this.btnThickness.Size = new System.Drawing.Size(75, 23);
+            this.btnThickness.TabIndex = 3;
+            this.btnThickness.Text = "Thickness";
+            this.btnThickness.UseVisualStyleBackColor = true;
+            this.btnThickness.Click += new System.EventHandler(this.btnThickness_Click);
+            // 
+            // thicknessTextBox
+            // 
+            this.thicknessTextBox.Location = new System.Drawing.Point(93, 444);
+            this.thicknessTextBox.Name = "thicknessTextBox";
+            this.thicknessTextBox.Size = new System.Drawing.Size(98, 20);
+            this.thicknessTextBox.TabIndex = 4;
+            // 
+            // txtImageUrl
+            // 
+            this.txtImageUrl.Location = new System.Drawing.Point(278, 446);
+            this.txtImageUrl.Name = "txtImageUrl";
+            this.txtImageUrl.Size = new System.Drawing.Size(349, 20);
+            this.txtImageUrl.TabIndex = 5;
+            // 
+            // btnInsertImage
+            // 
+            this.btnInsertImage.Location = new System.Drawing.Point(633, 444);
+            this.btnInsertImage.Name = "btnInsertImage";
+            this.btnInsertImage.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertImage.TabIndex = 6;
+            this.btnInsertImage.Text = "Insert Image";
+            this.btnInsertImage.UseVisualStyleBackColor = true;
+            this.btnInsertImage.Click += new System.EventHandler(this.btnInsertImage_Click);
+            // 
+            // WhiteboardClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 479);
+            this.Controls.Add(this.btnInsertImage);
+            this.Controls.Add(this.txtImageUrl);
+            this.Controls.Add(this.thicknessTextBox);
+            this.Controls.Add(this.btnThickness);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.panel1);
+            this.Name = "WhiteboardClient";
+            this.Text = "WhiteboardClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ lab06/WhiteboardClient.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace lab06
+{
+    public partial class WhiteboardClient : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Graphics g;
+        private bool drawing = false;
+        private Point previousPoint;
+        private Pen currentPen;
+
+        public WhiteboardClient()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            client = new TcpClient("127.0.0.1", 5000);
+            stream = client.GetStream();
+
+            Thread receiveThread = new Thread(ReceiveData);
+            receiveThread.Start();
+            currentPen = new Pen(Color.Black, 2);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawing = true;
+            previousPoint = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                g.DrawLine(currentPen, previousPoint, e.Location);
+                SendDrawData(previousPoint, e.Location);
+                previousPoint = e.Location;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+        }
+
+        private void SendDrawData(Point start, Point end)
+        {
+            MemoryStream ms = new MemoryStream();
+            BinaryWriter bw = new BinaryWriter(ms);
+            bw.Write(start.X);
+            bw.Write(start.Y);
+            bw.Write(end.X);
+            bw.Write(end.Y);
+            bw.Write(currentPen.Color.ToArgb());
+            bw.Write(currentPen.Width);
+            byte[] data = ms.ToArray();
+            stream.Write(data, 0, data.Length);
+        }
+
+        private void ReceiveData()
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                MemoryStream ms = new MemoryStream(buffer, 0, bytesRead);
+                BinaryReader br = new BinaryReader(ms);
+                int startX = br.ReadInt32();
+                int startY = br.ReadInt32();
+                int endX = br.ReadInt32();
+                int endY = br.ReadInt32();
+                Color penColor = Color.FromArgb(br.ReadInt32());
+                float penWidth = br.ReadSingle();
+                Invoke((MethodInvoker)delegate
+                {
+                    Pen pen = new Pen(penColor, penWidth);
+                    g.DrawLine(pen, new Point(startX, startY), new Point(endX, endY));
+                });
+            }
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(panel1.Width, panel1.Height);
+            panel1.DrawToBitmap(bitmap, new Rectangle(0, 0, panel1.Width, panel1.Height));
+            bitmap.Save("whiteboard.png", System.Drawing.Imaging.ImageFormat.Png);
+            client.Close();
+            this.Close();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentPen.Color = colorDialog.Color;
+            }
+        }
+
+        private void btnThickness_Click(object sender, EventArgs e)
+        {
+            float thickness;
+            if (float.TryParse(thicknessTextBox.Text, out thickness))
+            {
+                currentPen.Width = thickness;
+            }
+        }
+
+        private void btnInsertImage_Click(object sender, EventArgs e)
+        {
+            string imageUrl = txtImageUrl.Text;
+            try
+            {
+                Image image = Image.FromStream(new MemoryStream(new System.Net.WebClient().DownloadData(imageUrl)));
+                g.DrawImage(image, new Point(0, 0)); 
+                SendImageData(image);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load image: " + ex.Message);
+            }
+        }
+
+        private void SendImageData(Image image)
+        {
+            MemoryStream ms = new MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            byte[] data = ms.ToArray();
+            ms = new MemoryStream();
+            BinaryWriter bw = new BinaryWriter(ms);
+            bw.Write(data.Length);
+            bw.Write(data);
+            byte[] buffer = ms.ToArray();
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
